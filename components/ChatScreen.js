c8528191c0424cad9c05c0c3fce743f7a3e217ea
Abhishek Singh <writetoabhishek07@gmail.com>
2021-03-26 08:37:26 +0530

    structure Message Component

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -2,14 +2,45 @@ import { Avatar, Icon, IconButton } from "@material-ui/core";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import MicIcon from '@material-ui/icons/Mic'
+
 import AttachFileIcon from '@material-ui/icons/AttachFile'
 import styled from "styled-components"
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import Message  from "./Message";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useState } from "react";
+import firebase from "firebase"
 
 function ChatScreen({chat, messages}) {
-
     const [user] = useAuthState(auth);
+
+    const [input,setInput] = useState("");
     const router = useRouter();
+    const [messagesSnapshot] = useCollection(db.collection('chats').doc(router.query.id).collection('messages').orderBy('timestamp', "asc"));
+
+    const showMessages = () => {
+        if(messagesSnapshot) {
+            return messagesSnapshot.docs.map(message => (
+                <Message
+                key = { message.id}
+                user = { message.data.user }
+                message = {{
+                    ...message.data(),
+                    timestamp: message.data().timestamp?.toDate().getTime(),
+                }}
+                />
+            ))
+        }
+        else {
+            return JSON.parse(messages).map(message => (
+                <Message key={ message.id } user={ message.user }
+                message={ message} />
+            ))
+        }
+    }
 
     return (
         <Container>
@@ -30,16 +61,61 @@ function ChatScreen({chat, messages}) {
             </Header>
 
             <MessageContainer>
-                {/* show messages */}
+                {showMessages()}
+
                 <EndOfMessage/>
             </MessageContainer>
+
+            <InputContainer>
+                <InsertEmoticonIcon/>
+                <Input value = {input} onChange={e => setInput(e.target.value) } />
+                <button hidden disabled={!input} type = "submit" onClick={sendMessage} >Send Message</button>
+                <MicIcon/>
+            </InputContainer>
+
         </Container>
     )
+}
+
+const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("users").doc(user.uid).set({
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+    }, {merge: true})
+    db.collection("chats").doc(router.query.id).collection('messages').add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user.email, 
+        photoURL: user.photoURL,
+    })
+
+    setInput("");
 }
 
 export default ChatScreen
 
 const Container = styled.div`
+`;
+
+const InputContainer = styled.form`
+display: flex;
+align-items: center;
+padding: 10px;
+position: sticky;
+bottom: 0;
+background-color:white;
+z-index: 100;
+`;
+
+const Input = styled.input`
+flex: 1;
+outline: 0;
+border-radius: 10px;
+padding: 20px;
+background-color:whitesmoke;
+border: none;
+margin-left: 15px;
+margin-right: 15px;
 `;
 
 const Header = styled.div`
@@ -67,7 +143,11 @@ const HeaderInformation = styled.div`
         color: gray;
     }
 `
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+    padding:30px;
+    background-color:#e5ded8;
+    min-height: 90vh;
+`;
 
 const EndOfMessage = styled.div``;
 
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -64,7 +64,18 @@ function Sidebar() {
 export default Sidebar
 
 const Container = styled.div`
+    flex: 0.45;
+    border-right: 1px solid whitesmoke;
+    height:100vh;
+    min-width:300px;
+    max-width: 350px;
+    overflow-y: scroll;
 
+    ::-webkit-scrollbar {
+        display:none;
+    }
+    --ms-overflow-style: none; /*IE and Edge */
+    scrollbar-width: none; /*FireFox */
 `;
 
 const Search = styled.div`
