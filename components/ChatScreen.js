9db2f690bf657c08bc35c028efd91246f4f439b6
Abhishek Singh <writetoabhishek07@gmail.com>
2021-03-26 07:48:08 +0530

    complete Chat Screen Header Component

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,38 @@
+import { Avatar, Icon, IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
 import styled from "styled-components"
+import { auth } from "../firebase";
 
-function ChatScreen() {
+function ChatScreen({chat, messages}) {
+
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+
     return (
         <Container>
-            <h1>This is a Chat</h1>
+            <Header>
+                <Avatar/>
+                <HeaderInformation>
+                    <h3>Email</h3>
+                    <p>Last Seen ...</p>
+                </HeaderInformation>
+                <HeaderIcons>
+                    <IconButton>
+                        <AttachFileIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </HeaderIcons>
+            </Header>
+
+            <MessageContainer>
+                {/* show messages */}
+                <EndOfMessage/>
+            </MessageContainer>
         </Container>
     )
 }
@@ -12,3 +41,34 @@ export default ChatScreen
 
 const Container = styled.div`
 `;
+
+const Header = styled.div`
+    position:sticky;
+    background-color:white;
+    z-index:100;
+    top: 0;
+    display:flex;
+    padding: 11px;
+    height:80px;
+    align-items:center;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+    margin-left: 15px;
+    flex: 1;
+
+    >h3{
+        margin-bottom: 3px;
+    }
+
+    >p {
+        font-size: 14px;
+        color: gray;
+    }
+`
+const MessageContainer = styled.div``;
+
+const EndOfMessage = styled.div``;
+
+const HeaderIcons = styled.div``;
